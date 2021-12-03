9ae48695de64002372baf2f4f8a0202bf63edc7d
kadiremr3 <61358759+kadiremr3@users.noreply.github.com>
2021-12-03 13:50:22 +0300

    Add files via upload

---
+++ Adwise/Advice.swift
@@ -0,0 +1,12 @@
+//
+//  Advice.swift
+//  Adwiser
+//
+//  Created by Kadir Emre on 3.12.2021.
+//
+
+import Foundation
+
+struct Advice: Codable {
+    let slip: Slip
+}
---
+++ Adwise/MainVC.swift
@@ -0,0 +1,36 @@
+//
+//  MainVC.swift
+//  Adwiser
+//
+//  Created by Kadir Emre on 3.12.2021.
+//
+
+import UIKit
+import DWAnimatedLabel
+
+class MainVC: UIViewController {
+    
+    @IBOutlet var adviceLabel: DWAnimatedLabel!
+    var adviceManager = AdviceManager()
+    var shownMessage  = "Next Text"
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        adviceLabel.animationType = .fade
+        
+        
+    }
+        
+        @IBAction func getAdviceButtonTapped(_ sender: UIButton) {
+            
+            adviceManager.getAdvice()
+            adviceLabel.startAnimation(duration: 3, nextText: shownMessage) {
+                self.adviceLabel.text = self.adviceManager.mySlip?.advice
+            }
+                        
+            
+
+    }
+}
+
